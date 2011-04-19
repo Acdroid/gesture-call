3d9c86551da83efebf286875f2bd6e41a07c68eb
marcostruji@gmail.com <marcostruji@gmail.com@e919edf4-2f75-2e4d-1a48-4aa31d0f88cd>
2011-04-19 13:54:03 +0000

    Ahora llama al hacer gesto Agregado listado de gestos para editar o borrar. Añadido menu al main.

--- trunk/Gesture Call/src/ac/gestureCall/ui/main.java
+++ trunk/Gesture Call/src/ac/gestureCall/ui/main.java
@@ -1,18 +1,28 @@
 package ac.gestureCall.ui;
 
+
 import ac.gestureCall.R;
 import ac.gestureCall.ui.contactos.ListContact;
+import ac.gestureCall.ui.gestos.GestureBuilderActivity;
 import ac.gestureCall.util.config.AppConfig;
 import ac.gestureCall.util.gestures.GesturesRecognizer;
 import ac.gestureCall.util.mToast.mToast;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,6 +31,7 @@ public class main extends Activity {
 	public static final String NO_PREDICCION = "Sin_Resultado";
 	public static final int RESULT_OK = 0;
 	public static final int ID = 0;
+	public static final int DIALOG_SALIR = 0;
 	
 	public GestureOverlayView overlay;
 	public static GesturesRecognizer gr;
@@ -37,6 +48,8 @@ public class main extends Activity {
 	};
 	
 	
+	public Context mContext;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +60,8 @@ public class main extends Activity {
     
     
     private void init(){
+    	
+    	mContext = this;
     	
     	AppConfig ap = new AppConfig(this, AppConfig.NAME);
     	overlay = (GestureOverlayView)findViewById(R.id.gestures);
@@ -62,12 +77,74 @@ public class main extends Activity {
     }
     
     
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		@SuppressWarnings("unused")
+		Dialog dialog;
+		switch(id) {
+		case DIALOG_SALIR:
+			AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+			builder.setMessage(mContext.getResources().getString(R.string.di_salir))
+			.setCancelable(false)
+			.setPositiveButton(mContext.getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					//Comunicador.this.finish();
+					getStore().save();
+					((Activity) mContext).setResult(RESULT_OK);
+					((Activity) mContext).finish();
+				}
+			})
+			.setNegativeButton(mContext.getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
+			AlertDialog alert = builder.create();
+			return alert;
+		default:
+			return dialog = null;
+		}
+	}
+    
+    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_escritorio, menu);
+		return true;
+	}
+    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.me_salir:
+			showDialog(DIALOG_SALIR);
+			return true;
+		case R.id.me_gestures:
+			Intent i = new Intent(main.this,GestureBuilderActivity.class);
+			mContext.startActivity(i);
+			return true;
+		case R.id.me_opciones:
+			mToast.Make(this, "Pulsado opciones en general", 0);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	
+	
     public void call(String prediccion){
     	
     	if (prediccion.equals("")){
     		mToast.Make(this, "No se ha reconocido el gesto", 0);
     		return;
     	}
+    	
+    	String url = "tel:" + prediccion;
+        Intent i = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+        startActivityForResult(i,ID);
     	
     	mToast.Make(this, "Quieres llamar a : " + prediccion, 0);
 
