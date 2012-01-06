9f9508ef707d74f25ebc841a120bd226279e6fbb
marcostruji@gmail.com <marcostruji@gmail.com@e919edf4-2f75-2e4d-1a48-4aa31d0f88cd>
2012-01-06 21:40:39 +0000

    Facebook Arreglado
    Mejoras en la interfaz
    
    Animación para el botón donate

--- trunk/Gesture Call/src/ac/gestureCall/util/facebook/FacebookPostMessage.java
+++ trunk/Gesture Call/src/ac/gestureCall/util/facebook/FacebookPostMessage.java
@@ -1,23 +1,38 @@
 package ac.gestureCall.util.facebook;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import ac.gestureCall.util.facebook.SessionEvents.AuthListener;
+import ac.gestureCall.util.facebook.SessionEvents.LogoutListener;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.facebook.android.AsyncFacebookRunner;
 import com.facebook.android.DialogError;
 import com.facebook.android.Facebook;
 import com.facebook.android.Facebook.DialogListener;
 import com.facebook.android.FacebookError;
 
 
-public class FacebookPostMessage implements DialogListener{
+public class FacebookPostMessage implements DialogListener, AuthListener, LogoutListener{
+	private static final String FACEBOOK_ID ="145501312222682";
+
 	private Facebook facebookClient;
+	private AsyncFacebookRunner mAsyncFBRunner;
 	private Activity activity;
 	private String post="";
 	private String link="";
 	private String image="";
 	private String nameApp ="";
 	private Boolean posteado = false;
+	private String [] permisos = new String[] {"publish_stream"};
+	private Handler handler = new Handler();
+
+
 
 	public FacebookPostMessage (Activity act,String name, String mes, String l,String i){
 		activity = act;
@@ -27,37 +42,119 @@ public class FacebookPostMessage implements DialogListener{
 		image = i;
 
 		facebookClient = new Facebook("145501312222682");
-		String [] permisos = new String[] {"publish_stream", "publish_actions"};
-		facebookClient.authorize(activity, permisos, this);
-		Log.d("Gesture Call","Terminada petición autorizacion");
+		SessionStore.restore(facebookClient, activity);
+		SessionEvents.addAuthListener(this);
+		SessionEvents.addLogoutListener(this);
+		postMessageOnWall();
 
-		//Try to post the message. if cant, when autorize the app we will try other time
-		try
-		{
-			if (!posteado){
-				posteado = true;
-				Bundle parameters = new Bundle();
-				parameters.putString("message", post);// the message to post to the wall
 
-				if (!link.equals("") && (link != null))
-					parameters.putString("link",link);
 
-				if (!image.equals("") && (image != null))
-					parameters.putString("picture", image);
+		//
+		//		mAsyncFBRunner = new AsyncFacebookRunner(facebookClient);
+		//
+		//		facebookClient.authorize(activity, permisos, this);
+		//
+		//		Log.d("Gesture Call","Terminada petición autorizacion");
 
-				if (!nameApp.equals("") && (nameApp!= null))
-					parameters.putString("name", nameApp);    
-
-				facebookClient.dialog(activity, "stream.publish", parameters, this);
-			}
-		}
-		catch (Exception e)
-		{
-			Log.e("Gesture Call",e.getMessage());
-		}
-
-		Log.d("Gesture Call","Escrito comentario");
+		//		//Try to post the message. if cant, when autorize the app we will try other time
+		//		try
+		//		{
+		//			if (!posteado){
+		//				posteado = true;
+		//				Bundle parameters = new Bundle();
+		//				parameters.putString("message", post);// the message to post to the wall
+		//
+		//				if (!link.equals("") && (link != null))
+		//					parameters.putString("link",link);
+		//
+		//				if (!image.equals("") && (image != null))
+		//					parameters.putString("picture", image);
+		//
+		//				if (!nameApp.equals("") && (nameApp!= null))
+		//					parameters.putString("name", nameApp);    
+		//
+		//				facebookClient.dialog(activity, "stream.publish", parameters, this);
+		//			}
+		//		}
+		//		catch (Exception e)
+		//		{
+		//			Log.e("Gesture Call",e.getMessage());
+		//		}
+		//
+		//		Log.d("Gesture Call","Escrito comentario");
 	}
+
+
+	public void postMessageOnWall() {
+		if (facebookClient.isSessionValid()) {
+			Bundle parameters = new Bundle();
+			parameters.putString("message", post);
+
+			if (!link.equals("") && (link != null))
+				parameters.putString("link",link);
+
+			if (!image.equals("") && (image != null))
+				parameters.putString("picture", image);
+
+			if (!nameApp.equals("") && (nameApp!= null))
+				parameters.putString("name", nameApp);   
+
+			try {
+				String response = facebookClient.request("me/feed", parameters,"POST");
+				Log.d("Gesture Call" , " Response: " + response);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			login();
+		}
+	}
+
+
+
+	public void login() {
+		if (!facebookClient.isSessionValid()) {
+			facebookClient.authorize(this.activity, this.permisos,Facebook.FORCE_DIALOG_AUTH,this);
+		}
+	}
+
+
+
+	private void postMessageInThread() {
+		Thread t = new Thread() {
+			public void run() {
+
+				try {
+					postMessageOnWall();
+					handler.post(mUpdateFacebookNotification);
+				} catch (Exception ex) {
+					Log.e("Gesture Call", "Error sending msg",ex);
+				}
+			}
+		};
+		t.start();
+	}
+
+	private void clearCredentials() {
+		try {
+			facebookClient.logout(activity);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	final Runnable mUpdateFacebookNotification = new Runnable() {
+		public void run() {
+			Toast.makeText(activity, "Facebook updated !", Toast.LENGTH_LONG).show();
+			clearCredentials();
+		}
+	};
+
 
 	@Override
 	public void onCancel() {
@@ -67,45 +164,8 @@ public class FacebookPostMessage implements DialogListener{
 
 	@Override
 	public void onComplete(Bundle values) {
-		Log.i("Gesture Call","Autorización de facebook completada.");
-		if (values.isEmpty())
-		{
-			//"skip" clicked ?
-			return;
-		}
-
-		// if facebookClient.authorize(...) was successful, this runs
-		// this also runs after successful post
-		// after posting, "post_id" is added to the values bundle
-		// I use that to differentiate between a call from
-		// faceBook.authorize(...) and a call from a successful post
-		// is there a better way of doing this?
-		if (!values.containsKey("post_id"))
-		{
-			try{
-				if (!posteado){
-					posteado = true;
-					Bundle parameters = new Bundle();
-					parameters.putString("message", post);// the message to post to the wall
-
-					if (!link.equals("") && (link != null))
-						parameters.putString("link",link);
-
-					if (!image.equals("") && (image != null))
-						parameters.putString("picture", image);
-
-					if (!nameApp.equals("") && (nameApp!= null))
-						parameters.putString("name", nameApp);    
-
-					facebookClient.dialog(activity, "stream.publish", parameters, this);
-				}
-			}
-			catch (Exception e)
-			{
-				Log.e("Gesture Call",e.getMessage());
-			}
-		}
-
+		Log.d("Gesture Call","Autorización de facebook completada.");
+		postMessageInThread();
 	}
 
 	@Override
@@ -117,6 +177,37 @@ public class FacebookPostMessage implements DialogListener{
 	@Override
 	public void onFacebookError(FacebookError arg0) {
 		Log.e("Gesture Call","Error al intentar autorizar facebook, ERORR EN FACEBOOK.\n" + arg0.toString() + " " + arg0.getMessage());
+
+	}
+
+
+
+	//^^^^^^^  AuthListener ^^^^^^^^^^^^ //
+	@Override
+	public void onAuthSucceed() {
+		Log.d("Gesture Call", "login ok onAuth");
+		postMessageInThread();
+
+	}
+
+	@Override
+	public void onAuthFail(String error) {
+		Log.d("Gesture Call", "login mal onAuth");
+
+	}
+
+
+	//^^^^^^^  LogoutListener ^^^^^^^^^^^^ //
+
+	@Override
+	public void onLogoutBegin() {
+		Log.d("Gesture Call", "logout ok onAuth");
+
+	}
+
+	@Override
+	public void onLogoutFinish() {
+		Log.d("Gesture Call", "logout mal onAuth");
 
 	}
 
